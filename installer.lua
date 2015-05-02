9df20d2a3c3a8797caa5aa176c028b113c2e97fa
MCFunRide <funridedoesminecraft@gmail.com>
2015-05-02 21:20:40 +0100

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,99 +1,106 @@
-os.pullEvent = os.pullEventRaw
-function clear()
-	term.setBackgroundColor(colors.black)
+local files = {
+	"https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/startup.lua"
+	"https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/monitor.lua"
+	"https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/mon.lua"
+	"https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/lock.lua"
+	"https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/core.lua"
+}
+
+local githubUser    = ""
+local githubRepo    = ""
+local githubBranch  = ""
+
+local installerName = "CubeCore Installer" -- if you need one, this will replace "Installer for user/repo, branch branch"
+
+local function clear()
 	term.clear()
-	term.setCursorPos(1,1)
+	term.setCursorPos(1, 1)
 end
 
-function done()
-               term.setTextColor(colors.red)
-	clear()
-	print("Done!")
-	print("Press Any Key")
-	os.pullEvent("key")
-	os.reboot()
+local function httpGet(url, save)
+	if not url then
+		error("not enough arguments, expected 1 or 2", 2)
+	end
+	local remote = http.get(url)
+	if not remote then
+		return false
+	end
+	local text = remote.readAll()
+	remote.close()
+	if save then
+		local file = fs.open(save, "w")
+		file.write(text)
+		file.close()
+		return true
+	end
+	return text
 end
 
---function gh(url, name)
-	--f = fs.open(name, "w")
-	--f.write(http.get(name).readAll)
-	--f.close()
-	--sleep(1)
---end
-
-function install()
-	--Make Url Here!
-	fs.makeDir(".sertexsecurity")
-	monitor = http.get("https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/monitor.lua")
-	
-	core = http.get("https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/core.lua")
-	
-	lock = http.get("https://github.com/CubeSpaceCC/CubeCore/raw/master/lock.lua")
-	
-	mon = http.get("https://github.com/CubeSpaceCC/CubeCore/raw/master/mon.lua")
-	
-
-	
-	startup = http.get("https://github.com/CubeSpaceCC/CubeCore/raw/master/startup.lua")
-	
-	
-	
-	config = http.get("https://raw.githubusercontent.com/CubeSpaceCC/CubeCore/master/.sertexsecurity/config")
-	
-	sertexsecurity = http.get("https://github.com/CubeSpaceCC/CubeCore/raw/master/.sertexsecurity/udb/Core")
-	
-	
-	
-	f = fs.open("monitor", "w")
-	f.write(monitor.readAll)
-	f.close()
-	
-	f = fs.open("core", "w")
-	f.write(core.readAll)
-	f.close()
-	
-	f = fs.open("lock", "w")
-	f.write(cm.readAll)
-	f.close()
-	
-	f = fs.open("mon", "w")
-	f.write(cmd.readAll)
-	f.close()
-	
-	f = fs.open("startup", "w")
-	f.write(startup.readAll)
-	f.close()
-	
-	shell.run("mkdir .sertexsecurity")
-	f = fs.open("/.sertexsecurity/config","w")
-	f.write(config.readAll)
-	f.close()
-	
-	shell.run("mkdir .sertexsecurity/udb")
-	f = fs.open("Core", "w")
-	f.write(sertexsecurity.readAll)
-	f.close()
-	
-	done()
+local function get(user, repo, bran, path, save)
+	if not user or not repo or not bran or not path then
+		error("not enough arguments, expected 4 or 5", 2)
+	end
+    local url = "https://raw.github.com/"..user.."/"..repo.."/"..bran.."/"..path
+	local remote = http.get(url)
+	if not remote then
+		return false
+	end
+	local text = remote.readAll()
+	remote.close()
+	if save then
+		local file = fs.open(save, "w")
+		file.write(text)
+		file.close()
+		return true
+	end
+	return text
 end
-term.setBackgroundColor(colors.white)
+
+local function getFile(file, target)
+	return get(githubUser, githubRepo, githubBranch, file, target)
+end
+
+shell.setDir("")
+
 clear()
-term.setTextColor(colors.red)
-print("CubeCore 1.0 Installer")
 
-print("Install CubeCore 1.0? Y or N")
-
-while true do
-	local id, key = os.pullEvent("key")
-	
-	if key == 21 then
-		install()
-		break
-	end
-	
-	if key == 49 then
-		printError("Terminated")
-		shell.run("shell")
-	end
-	sleep(0)
+print(installerName or ("Installer for " .. githubUser .. "/" .. githubRepo .. ", branch " .. githubBranch))
+print("Getting files...")
+local fileCount = 0
+for _ in pairs(files) do
+	fileCount = fileCount + 1
 end
+local filesDownloaded = 0
+
+local w, h = term.getSize()
+
+for k, v in pairs(files) do
+	term.setTextColor(colors.black)
+	term.setBackgroundColor(colors.white)
+	clear()
+	term.setCursorPos(2, 2)
+	print(installerName or ("Installer for " .. githubUser .. "/" .. githubRepo .. ", branch " .. githubBranch))
+	term.setCursorPos(2, 4)
+	print("File: "..v)
+	term.setCursorPos(2, h - 1)
+	print(tostring(math.floor(filesDownloaded / fileCount * 100)).."% - "..tostring(filesDownloaded + 1).."/"..tostring(fileCount))
+	local ok = k:sub(1, 4) == "ext:" and httpGet(k:sub(5), v) or getFile(k, v)
+	if not ok then
+		if term.isColor() then
+			term.setTextColor(colors.red)
+		end
+		term.setCursorPos(2, 6)
+		print("Error getting file:")
+		term.setCursorPos(2, 7)
+		print(k)
+		sleep(1)
+	end
+	filesDownloaded = filesDownloaded + 1
+end
+clear()
+term.setCursorPos(2, 2)
+print("Press any key to continue.")
+local w, h = term.getSize()
+term.setCursorPos(2, h-1)
+print("100% - "..tostring(filesDownloaded).."/"..tostring(fileCount))
+os.pullEvent("key")
